328ea04d27e27adfc98da16d8a5d0de371939708
jordanonuma <jordanonuma@outlook.com>
2020-06-27 08:36:58 -1000

    Added frame of private function that will return a uint. It views on chain data but doesn't modify it.

--- Contract.sol
+++ Contract.sol
@@ -15,4 +15,8 @@ contract ZombieFactory {
         zombies.push(Zombie(_name, _dna));
     } //end function _createZombie()
 
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        
+    } //end function _generateRandomDna()
+
 } //end contract ZombieFactory{}
