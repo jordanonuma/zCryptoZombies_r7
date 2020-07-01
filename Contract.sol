21230eb8cb3b6d1f6c4fef25412f5b81df365981
jordanonuma <jordanonuma@outlook.com>
2020-06-30 17:47:09 -1000

    Declared zombie ID in the array.

--- Contract.sol
+++ Contract.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
     event newZombie(uint zombieId, string name, uint dna);
-    
+
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
@@ -15,6 +15,7 @@ contract ZombieFactory {
 
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
+        uint id = zombies.push() - 1;
     } //end function _createZombie()
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
