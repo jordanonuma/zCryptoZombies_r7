46ea15f467eadc482ca7c3dc15625d5c3be4e66d
jordanonuma <jordanonuma@outlook.com>
2020-07-01 17:22:32 -1000

    Declared zombie-owner mapping.

--- Contract.sol
+++ Contract.sol
@@ -13,6 +13,8 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    mapping (uint => address) public zombieToOwner;
+
     function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
         emit NewZombie(id, _name, _dna);
