ef546b95eaba3337021ca8b510e0f0198215b7aa
jordanonuma <jordanonuma@outlook.com>
2020-07-07 23:22:40 -1000

    Truncated the DNA string.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -6,5 +6,7 @@ contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
+
+        _targetDna = _targetDna % dnaModulus;
     } //end function feedAndMultiply()
 } //end contract ZombieFeeding{}
