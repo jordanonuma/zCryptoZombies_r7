310688032ec7227b6a6070a4ea8a658178bd3dca
jordanonuma <jordanonuma@outlook.com>
2020-07-10 17:25:28 -1000

    Added function that KittyInterface{} will read in from the crypto kitty contract.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -2,7 +2,18 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./ZombieFactory.sol";
 
 contract KittyInterface{
-    
+    function getKitty(uint256 _id) external view returns (
+        bool isGestating,
+        bool isReady,
+        uint256 cooldownIndex,
+        uint256 nextActionAt,
+        uint256 siringWithId,
+        uint256 birthTime,
+        uint256 matronId,
+        uint256 sireId,
+        uint256 generation,
+        uint256 genes
+    ); //end function getKitty()
 } //end contract KittyInterface{}
 
 contract ZombieFeeding is ZombieFactory {
