e3b4cdaa01f467f3bde4630560d456366f0d21c7
jordanonuma <jordanonuma@outlook.com>
2020-07-11 09:45:47 -1000

    Defined crypto kitty address.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -17,7 +17,7 @@ contract KittyInterface{
 } //end contract KittyInterface{}
 
 contract ZombieFeeding is ZombieFactory {
-
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
