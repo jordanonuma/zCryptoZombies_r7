b65008875b058b05eee241bd186d1746a8451902
jordanonuma <jordanonuma@outlook.com>
2020-07-12 09:35:40 -1000

    Fixed variable reference.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -31,7 +31,7 @@ contract ZombieFeeding is ZombieFactory {
 
     function feedOnKitty(uint _zombieId, uint _kittyId) public {
         uint kittyDna;
-        (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kitty);
+        (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
         feedAndMultiply(_zombieId, kittyDna);
     } //end function feedOnKitty()
 } //end contract ZombieFeeding{}
