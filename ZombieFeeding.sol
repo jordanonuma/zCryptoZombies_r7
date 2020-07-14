a1c8b23bbc965348f7373178a71d2aa467ea0f40
jordanonuma <jordanonuma@outlook.com>
2020-07-13 20:27:08 -1000

    Revised math.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -28,7 +28,7 @@ contract ZombieFeeding is ZombieFactory {
         uint newDna = (myZombie.dna + _targetDna)/2;
 
         if(keccak256(abi.encodePacked(_species))==keccak256(abi.encodePacked("kitty"))) {
-            newDna = newDna % 100 + 99;
+            newDna = newDna - newDna % 100 + 99;
         } //end if()
         _createZombie("NoName", newDna);
     } //end function feedAndMultiply()
