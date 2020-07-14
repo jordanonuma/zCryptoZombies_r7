9fe6ae720c8217ad1d734ee5f6fb3092975497ea
jordanonuma <jordanonuma@outlook.com>
2020-07-13 20:23:01 -1000

    Checks if species is a kitty.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -26,6 +26,10 @@ contract ZombieFeeding is ZombieFactory {
 
         _targetDna = _targetDna % dnaModulus;
         uint newDna = (myZombie.dna + _targetDna)/2;
+
+        if(keccak256(abi.encodePacked(_species))==keccak256(abi.encodePacked("kitty"))) {
+      
+        } //end if()
         _createZombie("NoName", newDna);
     } //end function feedAndMultiply()
 
