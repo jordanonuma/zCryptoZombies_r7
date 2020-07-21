2bcf4a896591a3f6d2403f9944edf5106559a7a0
jordanonuma <jordanonuma@outlook.com>
2020-07-20 20:38:28 -1000

    Sends through boolean check if cooldownTime has passed.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -34,6 +34,7 @@ contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
+        require(_isReady(myZombie));
 
         _targetDna = _targetDna % dnaModulus;
         uint newDna = (myZombie.dna + _targetDna)/2;
