02fdee902fc58ded2829652ab32ca1e3d8204f3f
jordanonuma <jordanonuma@outlook.com>
2020-07-21 20:02:55 -1000

    Checks for minimum level then goes back to function's logic.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -3,6 +3,7 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
     modifier aboveLevel(uint _level, uint _zombieId) {
-      
+        require(zombies[zombieId].level >= _level);
+        _;
     } //end modifier aboveLevel()
 } //end contract ZombieHelper{}
