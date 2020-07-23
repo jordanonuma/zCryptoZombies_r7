4e715f647b7a8724a9380cff677ae23969972698
jordanonuma <jordanonuma@outlook.com>
2020-07-22 18:12:38 -1000

    Sets new DNA.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -14,5 +14,6 @@ contract ZombieHelper is ZombieFeeding {
 
     function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
         require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].dna = _newDna;
     } //end function changeDna() 
 } //end contract ZombieHelper{}
