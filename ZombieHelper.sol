db9791422fb9f6a5c0f49d81e68768915f9ae952
jordanonuma <jordanonuma@outlook.com>
2020-07-24 17:46:50 -1000

    Declared new uint[] array called 'result' that temporarily stores the zombies of an owner. Length has been set to total zombies.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -18,6 +18,6 @@ contract ZombieHelper is ZombieFeeding {
     } //end function changeDna()
 
     function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
-      
+        uint[] memory result = new uint[](ownerZombieCount[_owner]);
     } //end function getZombiesByOwner()
 } //end contract ZombieHelper{}
