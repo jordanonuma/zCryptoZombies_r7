e37985f21d70586d2e115edcbb3e3aaea65e5bfc
jordanonuma <jordanonuma@outlook.com>
2020-07-26 10:47:31 -1000

    Defined fee to level up zombie.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -2,6 +2,9 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
+
+    uint levelUpFee = 0.001 ether;
+    
     modifier aboveLevel(uint _level, uint _zombieId) {
         require(zombies[_zombieId].level >= _level);
         _;
