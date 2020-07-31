af80d842c214cad46ce1ac416717d18426979ffc
jordanonuma <jordanonuma@outlook.com>
2020-07-30 20:16:45 -1000

    Set the average probability of victory.

--- ZombieAttack.sol
+++ ZombieAttack.sol
@@ -3,6 +3,8 @@ import "./zombiehelper.sol";
 
 contract ZombieAttack is ZombieHelper {
     uint randNonce = 0;
+    uint attackVictoryProbability = 70;
+    
     function randMod(uint _modulus) internal returns(uint) {
         randNonce++;
         return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
