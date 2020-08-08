053558d918bc77f3893e8725733e63d65332c463
jordanonuma <jordanonuma@outlook.com>
2020-08-08 09:27:32 -1000

    Returns number of zombies user has.

--- ZombieOwnership.sol
+++ ZombieOwnership.sol
@@ -3,5 +3,21 @@ import "./ZombieAttack.sol";
 import "./ERC721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
+
+    function balanceOf(address _owner) external view returns (uint256) {
+    
+    } //end function balanceOf()
+
+    function ownerOf(uint256 _tokenId) external view returns (address) {
+    
+    } //end function ownerOf()
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+
+    } //end function transferFrom()
+
+    function approve(address _approved, uint256 _tokenId) external payable {
+
+    } //end function approve()
     
 } //end contract ZombieOwnership{}
