76e76b2187b20b33a9d1942cfbce6a30bfa3f2de
jordanonuma <jordanonuma@outlook.com>
2020-08-29 10:08:06 -1000

    Initialized gameState.

--- zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
+++ zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
@@ -6,6 +6,7 @@ contract ExampleGame is ZBGameMode  {
 
     function beforeMatchStart(bytes serializedGameState) external {
         GameState memmory gameState; //GameState instance called 'gameState'
+        gameState.init(serializedGameState);
     } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
