9e3e1694669cc5c223a3132b48c931c25143fe08
jordanonuma <jordanonuma@outlook.com>
2020-08-29 10:07:26 -1000

    Declared GameState instance called 'gameState'.

--- zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
+++ zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
@@ -5,7 +5,7 @@ import "./ZB/ZBGameMode.sol";
 contract ExampleGame is ZBGameMode  {
 
     function beforeMatchStart(bytes serializedGameState) external {
-        
+        GameState memmory gameState; //GameState instance called 'gameState'
     } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
