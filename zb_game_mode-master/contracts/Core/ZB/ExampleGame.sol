0e6b95152c7cd2a91d57cce5e99de255d47f91d7
jordanonuma <jordanonuma@outlook.com>
2020-08-30 10:12:42 -1000

    Declared custom data type variable called changes and initialized it.

--- zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
+++ zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
@@ -7,6 +7,9 @@ contract ExampleGame is ZBGameMode  {
     function beforeMatchStart(bytes serializedGameState) external {
         GameState memory gameState; //GameState instance called 'gameState'
         gameState.init(serializedGameState);
+
+        ZBSerializer.SerializedGameStateChanges memory changes;
+        changes.init();
     } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
