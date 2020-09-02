bc6b094b9ee7a914d854511fe949024f0773bcf2
jordanonuma <jordanonuma@outlook.com>
2020-09-01 19:15:24 -1000

    Defined mapping for cards not allowed in match.

--- zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
+++ zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
@@ -4,6 +4,8 @@ import "./ZB/ZBGameMode.sol";
 
 contract ExampleGame is ZBGameMode  {
 
+    mapping (stirng => bool) internal bannedCards;
+    
     function beforeMatchStart(bytes serializedGameState) external {
         GameState memory gameState; //GameState instance called 'gameState'
         gameState.init(serializedGameState);
