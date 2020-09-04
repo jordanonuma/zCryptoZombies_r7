d624c1241698bf69798fdb410765d70815c102b8
jordanonuma <jordanonuma@outlook.com>
2020-09-03 19:43:20 -1000

    Increments card counter.

--- zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
+++ zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
@@ -58,6 +58,7 @@ contract ExampleGame is ZBGameMode  {
             for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
                 if(isLegalCard(gameState.playerStates[i].cardsInDeck[j])) {
                     newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
+                    cardCount++;
                 } //end if()
             } //end for(j)
         } //end for(i)
