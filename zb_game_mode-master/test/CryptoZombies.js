2e38ec19c9036ca58747b1568f55e496209d09c6
jordanonuma <jordanonuma@outlook.com>
2020-09-09 17:26:58 -1000

    Defined contract abstraction 'contractInstance' to root calls to Solidity functions.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -3,6 +3,6 @@ const CryptoZombies = artifacts.require("CryptoZombies");
 contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
     it("should be able to create a new zombie", async () => {
-        
+        const contractInstance = await CryptoZombies.new();
     }) //end it()
 }) //end contract("", ()=>{})
