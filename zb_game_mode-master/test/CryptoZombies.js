94fdf8569267c066bf3fd8de5f6fb0bb00d4a71c
jordanonuma <jordanonuma@outlook.com>
2020-09-10 17:33:41 -1000

    Tests the solidity createRandomZombie() function.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -4,5 +4,6 @@ contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
     it("should be able to create a new zombie", async () => {
         const contractInstance = await CryptoZombies.new();
+        const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
     }) //end it()
 }) //end contract("", ()=>{})
