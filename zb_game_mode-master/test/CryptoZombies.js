a5d2598dc99932e68bf7ad5c36daa9aa9d54562d
jordanonuma <jordanonuma@outlook.com>
2020-09-11 20:25:31 -1000

    Declares JS variable that represents a new instance of the contract being tested.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -2,6 +2,8 @@ const CryptoZombies = artifacts.require("CryptoZombies");
 
 contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
+    var contractInstance;
+    
     it("should be able to create a new zombie", async () => {
         const contractInstance = await CryptoZombies.new();
         const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
