6a1a0ee2f45bdfd68e7a53f4d4b746539b15eb8a
jordanonuma <jordanonuma@outlook.com>
2020-09-12 10:45:26 -1000

    Defined shouldThrow() for the second zombie.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -16,6 +16,6 @@ contract("CryptoZombies", (accounts) => {
 
     it("should not allow two zombies", async () => {
         await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
-        await utils.shouldThrow(contractInstance.createRandomZombie());
+        await utils.shouldThrow(contractInstance.createRandomZombie(zombieNames[1], {from: alice}));
     }) //end it()
 }) //end contract("", ()=>{})
