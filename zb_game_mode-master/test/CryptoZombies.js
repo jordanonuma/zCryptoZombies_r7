a94a5a8b7bd0ca6457ede271e4a60710740e76c2
jordanonuma <jordanonuma@outlook.com>
2020-09-15 19:51:31 -1000

    Calls solidity function that creates a zombie from Alice's account.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -21,7 +21,7 @@ contract("CryptoZombies", (accounts) => {
 
     xcontext("with the single-step transfer scenario", async() => {
         it("should transfer a  zombie", async() => {
-            
+            await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
         }) //end it()
     }) //end context()
 
