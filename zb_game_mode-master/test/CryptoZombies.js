65ed87cca8010f3038ca4d463587c9ab1f8f86e5
jordanonuma <jordanonuma@outlook.com>
2020-09-16 19:13:21 -1000

    Testing scenario where Bob calls transferFrom() to pull ERC721.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -34,7 +34,9 @@ contract("CryptoZombies", (accounts) => {
             const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
             const zombieId = result.logs[0].args.zombieId.toNumber();
             await contractInstance.approve(bob, zombieId, {from: alice});
-            
+            await contractInstance.transferFrom(alice, bob, zombieId, {from: bob});
+            const newOwner = await contractInstance.ownerOf(zombieId);
+            assert.equal(newOwner,bob);
         }) //end it()
         it("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
             
