731f5f43892c1757970406e40faad1619c9142d1
jordanonuma <jordanonuma@outlook.com>
2020-09-17 21:03:52 -1000

    Unflagged scenario for testing.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -38,13 +38,13 @@ contract("CryptoZombies", (accounts) => {
             const newOwner = await contractInstance.ownerOf(zombieId);
             assert.equal(newOwner,bob);
         }) //end it()
-        xit("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
+        it("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
             const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
             const zombieId = result.logs[0].args.zombieId.toNumber();
             await contractInstance.approve(bob, zombieId, {from: alice});
             await contractInstance.transferFrom(alice, bob, zombieId, {from: alice});
             const newOwner = await contractInstance.ownerOf(zombieId);
-            assert.equal(newOwner,bob);
+            assert.equal(newOwner, bob);
         }) //end it()
     }) //end context()
 }) //end contract("", ()=>{})
