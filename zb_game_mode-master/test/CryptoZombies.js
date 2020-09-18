e76b934e350c40ee50fbad285dd4c0f6b686e7fe
jordanonuma <jordanonuma@outlook.com>
2020-09-17 21:03:34 -1000

    Added case where Alice calls transferFrom().

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -39,7 +39,12 @@ contract("CryptoZombies", (accounts) => {
             assert.equal(newOwner,bob);
         }) //end it()
         xit("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
-            
+            const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+            const zombieId = result.logs[0].args.zombieId.toNumber();
+            await contractInstance.approve(bob, zombieId, {from: alice});
+            await contractInstance.transferFrom(alice, bob, zombieId, {from: alice});
+            const newOwner = await contractInstance.ownerOf(zombieId);
+            assert.equal(newOwner,bob);
         }) //end it()
     }) //end context()
 }) //end contract("", ()=>{})
