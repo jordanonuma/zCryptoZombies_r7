336e0a0930005f88cab7e43837a7b116100e0961
jordanonuma <jordanonuma@outlook.com>
2020-09-18 20:46:04 -1000

    Declared constants using functions in the JS helper functions.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -1,4 +1,7 @@
 const CryptoZombies = artifacts.require("CryptoZombies");
+const utils = require("./helpers/utils");
+const time = require("./helpers/time");
+const zombieNames = ["Zombie 1", "Zombie 2"];
 
 contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
