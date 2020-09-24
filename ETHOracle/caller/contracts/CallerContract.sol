40b1f6e48ac3d246d2a887aefb73bbeae8d7116a
jordanonuma <jordanonuma@outlook.com>
2020-09-23 18:56:00 -1000

    Added import to EthPriceOracle contract in a separate folder.

--- ETHOracle/caller/contracts/CallerContract.sol
+++ ETHOracle/caller/contracts/CallerContract.sol
@@ -1,4 +1,5 @@
 pragma solidity 0.5.0;
+import "../oracle/contracts/EthPriceOracle.sol";
 contract CallerContract {
     address private oracleAddress;
     function setOracleInstanceAddress(address _oracleInstanceAddress) public {
