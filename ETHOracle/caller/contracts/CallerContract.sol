3bcc309fd8841f187f4a1a3465626e47cb334ad6
jordanonuma <jordanonuma@outlook.com>
2020-09-24 17:35:53 -1000

    Defined new event that will let front end know when a new oracle address has been set.

--- ETHOracle/caller/contracts/CallerContract.sol
+++ ETHOracle/caller/contracts/CallerContract.sol
@@ -4,6 +4,7 @@ import "../../Ownable.sol";
 contract CallerContract is Ownable {
     EthPriceOracle private oracleInstance;
     address private oracleAddress;
+    event newOracleAddressEvent(address oracleAddress);
     function setOracleInstanceAddress(address _oracleInstanceAddress) public onlyOwner {
         oracleAddress = _oracleInstanceAddress;
         oracleInstance = EthPriceOracleInterface(oracleAddress);
