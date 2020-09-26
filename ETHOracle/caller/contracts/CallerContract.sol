c5f32f41b7460738797a67e638c7df1f19558aa9
jordanonuma <jordanonuma@outlook.com>
2020-09-25 21:45:25 -1000

    Added mapping for requests and event requests have been received.

--- ETHOracle/caller/contracts/CallerContract.sol
+++ ETHOracle/caller/contracts/CallerContract.sol
@@ -4,7 +4,11 @@ import "../../Ownable.sol";
 contract CallerContract is Ownable {
     EthPriceOracle private oracleInstance;
     address private oracleAddress;
+    mapping(uint256=>bool) myRequests;
+
     event newOracleAddressEvent(address oracleAddress);
+    event ReceivedNewRequestIdEvent(uint256 id);
+
     function setOracleInstanceAddress(address _oracleInstanceAddress) public onlyOwner {
         oracleAddress = _oracleInstanceAddress;
         oracleInstance = EthPriceOracleInterface(oracleAddress);
