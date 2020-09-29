264ae6ffb53e9c026e0c15eb5d0f4761f34aabd8
jordanonuma <jordanonuma@outlook.com>
2020-09-28 18:54:52 -1000

    Libraries not necessary to import currently.

--- ETHOracle/oracle/contracts/EthPriceOracle.sol
+++ ETHOracle/oracle/contracts/EthPriceOracle.sol
@@ -1,8 +1,8 @@
 pragma solidity 0.5.0;
 // import "../../../Ownable.sol";
-import "../access/Roles.sol";
-import "./CallerContractInterface.sol";
-import "../../../SafeMath.sol";
+//import "../access/Roles.sol";
+//import "./CallerContractInterface.sol";
+//import "../../../SafeMath.sol";
 
 contract EthPriceOracle {
     uint private randNonce = 0;
