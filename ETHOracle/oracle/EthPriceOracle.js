bd36e1527d909eb8fb93672225c5ab9dc1c887cc
jordanonuma <jordanonuma@outlook.com>
2020-10-03 11:37:12 -1000

    Revised for consistent event names.

--- ETHOracle/oracle/EthPriceOracle.js
+++ ETHOracle/oracle/EthPriceOracle.js
@@ -14,7 +14,7 @@ async function getOracleContract(web3js) {
 } //end async function getOracleContract()
 
 async function filterEvents(oracleContract, web3js) {
-    EthPriceOracle.events.getLatestEthPrice(async (err, event) => {
+    EthPriceOracle.events.GetLatestEthPrice(async (err, event) => {
         if (err) {
           console.error('Error on event', err)
           return
