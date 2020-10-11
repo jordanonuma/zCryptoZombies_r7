718dcee830a14b168dc862cc7cc2b54436d8f0e9
jordanonuma <jordanonuma@outlook.com>
2020-10-11 09:35:17 -1000

    Filters events and returns three important objects in array.

--- ETHOracle/oracle/EthPriceOracle.js
+++ ETHOracle/oracle/EthPriceOracle.js
@@ -93,4 +93,6 @@ async function setLatestEthPrice (oracleContract, callerAddress, ownerAddress, e
   async function init () {
     const {ownerAddress, web3js, client } = common.loadAccount(PRIVATE_KEY_FILE_NAME) 
     const oracleContract = oracleContract.methods.getOracleContract(web3js)
+    filterEvents(oracleContract, web3js)
+    return { oracleContract, ownerAddress, client}
   } //end async function init()
