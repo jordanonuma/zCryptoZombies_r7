8bed8b4e511f19c4cc5d44e62c63809a6ee3f0b3
jordanonuma <jordanonuma@outlook.com>
2020-10-19 20:12:00 -1000

    Added indendation for readability.

--- ETHOracle/oracle/contracts/EthPriceOracle.sol
+++ ETHOracle/oracle/contracts/EthPriceOracle.sol
@@ -5,27 +5,27 @@ import "./CallerContractInterface.sol";
 //import "../../../SafeMath.sol";
 
 contract EthPriceOracle { 
-  using Roles for Roles.Role;
-  Roles.Role private owners;
-  Roles.Role private oracles;
+    using Roles for Roles.Role;
+    Roles.Role private owners;
+    Roles.Role private oracles;
 
-  uint private randNonce = 0;
-  uint private modulus = 1000;
-  mapping(uint256=>bool) pendingRequests;
-  event GetLatestEthPriceEvent(address callerAddress, uint id);
-  event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
-  event AddOracleEvent(address oracleAddress);
+    uint private randNonce = 0;
+    uint private modulus = 1000;
+    mapping(uint256=>bool) pendingRequests;
+    event GetLatestEthPriceEvent(address callerAddress, uint id);
+    event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
+    event AddOracleEvent(address oracleAddress);
 
-  constructor (address _owner) public {
-      owners.add(_owner);
-  } //end constructor()
+    constructor (address _owner) public {
+        owners.add(_owner);
+    } //end constructor()
 
-  function addOracle(address _oracle) public {
-      require(owners.has(msg.sender), "Not an owner!");
-      require(!oracles.has(_oracle), "Already an oracle!");
-      oracles.add(_oracle);
-      emit AddOracleEvent(_oracle);
-  } //end function addOracle()
+    function addOracle(address _oracle) public {
+        require(owners.has(msg.sender), "Not an owner!");
+        require(!oracles.has(_oracle), "Already an oracle!");
+        oracles.add(_oracle);
+        emit AddOracleEvent(_oracle);
+    } //end function addOracle()
 
     function removeOracle(address _oracle) public {
         require(owners.has(msg.sender), "Not an owner!");
@@ -33,19 +33,19 @@ contract EthPriceOracle {
     
     } //end function removeOracle()
 
-  function getLatestEthPrice() public returns (uint256) {
-    randNonce++;
-    uint id = uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % modulus;
-    pendingRequests[id] = true;
-    emit GetLatestEthPriceEvent(msg.sender, id);
-    return id;
-  } //end function getLatestEthPrice()
-  function setLatestEthPrice(uint256 _ethPrice, address _callerAddress, uint256 _id) public onlyOwner {
-    require(pendingRequests[_id], "This request is not in my pending list.");
-    delete pendingRequests[_id];
-    CallerContracInterface callerContractInstance;
-    callerContractInstance = CallerContracInterface(_callerAddress);
-    callerContractInstance.callback(_ethPrice, _id);
-    emit SetLatestEthPriceEvent(_ethPrice, _callerAddress);
-  } //end function setLatestEthPrice()
+    function getLatestEthPrice() public returns (uint256) {
+        randNonce++;
+        uint id = uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % modulus;
+        pendingRequests[id] = true;
+        emit GetLatestEthPriceEvent(msg.sender, id);
+        return id;
+    } //end function getLatestEthPrice()
+    function setLatestEthPrice(uint256 _ethPrice, address _callerAddress, uint256 _id) public onlyOwner {
+        require(pendingRequests[_id], "This request is not in my pending list.");
+        delete pendingRequests[_id];
+        CallerContracInterface callerContractInstance;
+        callerContractInstance = CallerContracInterface(_callerAddress);
+        callerContractInstance.callback(_ethPrice, _id);
+        emit SetLatestEthPriceEvent(_ethPrice, _callerAddress);
+    } //end function setLatestEthPrice()
 } //end contract EthPriceOracle{}
