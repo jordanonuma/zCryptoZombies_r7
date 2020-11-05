f64a4569a820081f419db7e03e963d34628f9830
jordanonuma <jordanonuma@outlook.com>
2020-11-04 18:24:29 -1000

    Defined network.

--- zksync-tutorial/bob.js
+++ zksync-tutorial/bob.js
@@ -3,5 +3,6 @@
     const zksync = require('zksync')
     const utils = require('./utils')
   
+    export NETWORK_NAME=rinkeby
     const zkSyncProvider = await utils.getZkSyncProvider(zksync, process.env.NETWORK_NAME)
 })() //end async()
