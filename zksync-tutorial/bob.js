547f3a4be84263549ac2a4994b1ad1cca5a312dd
jordanonuma <jordanonuma@outlook.com>
2020-11-05 18:11:54 -1000

    Instantiates Bob's testnet wallet.

--- zksync-tutorial/bob.js
+++ zksync-tutorial/bob.js
@@ -6,4 +6,6 @@
     //may not be necessary: export NETWORK_NAME=rinkeby
     const zkSyncProvider = await utils.getZkSyncProvider(zksync, process.env.NETWORK_NAME)
     const ethersProvider = await utils.getEthereumProvider(ethers, process.env.NETWORK_NAME)
+
+    const bobRinkebyWallet = new ethers.Wallet(process.env.BOB_PRIVATE_KEY, ethersProvider)
 })() //end async()
