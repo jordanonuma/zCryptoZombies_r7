22363b78b2a0a204605248276a256e371506a6b0
jordanonuma <jordanonuma@outlook.com>
2020-11-09 18:21:50 -1000

    Removed closing parenthesis for syntax.

--- zksync-tutorial/alice.js
+++ zksync-tutorial/alice.js
@@ -28,7 +28,7 @@
     await utils.transfer(aliceZkSyncWallet, process.env.BOB_ADDRESS, amountToTransfer, transferFee, token, zksync, ethers)
 
     console.log('Withdrawing')
-    const withdrawalFee = await utils.getFee('Withdraw'), aliceRinkebyWallet.address, token, zkSyncProvider, ethers)
+    const withdrawalFee = await utils.getFee('Withdraw', aliceRinkebyWallet.address, token, zkSyncProvider, ethers)
     await utils.withdrawToEthereum(aliceZkSyncWallet, amountToWithdraw, withdrawalFee, token, zksync, ethers)
   })()
   
